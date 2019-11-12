25a7597b20c555bb3c107da1e417af5ea1542cc0
Solexplorer <50027175+Solexplorer@users.noreply.github.com>
2019-11-12 06:11:24 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -192,4 +192,4 @@ if __name__ == "__main__":        # on running python app.py
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    app.run(host='0.0.0.0')                    # run the flask app
+    app.run()                    # run the flask app
